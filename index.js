94d808f59d3a7f5741d819ba1e295c3f61870b15
Aaron Hegde <hegde.aaron@gmail.com>
2020-06-11 17:46:03 -0400

    finished counter

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+class Counter extends React.Component{
+        constructor(props) {
+            super(props);
+            this.state = {
+            value: 0
+            };
+        }
+
+    add = () => {
+        this.setState({ value: this.state.value + 1});
+    }
+
+    subtract = () => {
+        this.setState({ value: this.state.value - 1});
+    }
+
+    render = () => {
+        return  <div>
+                <button onClick={this.add}  className = "add">+</button>
+                <button onClick={this.subtract} className = "subtract">-</button>
+                <h1>{this.state.value}</h1>
+            </div>
+        
+    }
+
+}
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
